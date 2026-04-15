ddd5a7d439e3127edee84e4fc90f5a34a33448f0
amijun744 <kemalumer993@gmail.com>
2026-04-15 13:59:36 +0300

    first commit

--- script.js
+++ script.js
@@ -5,8 +5,30 @@ function scrollToSection(sectionId) {
     });
 }
 
-// Fake contact form handler
+// Contact form
 document.getElementById("contactForm").addEventListener("submit", function(e) {
     e.preventDefault();
     document.getElementById("formMessage").innerText = "Message sent successfully!";
+});
+
+// Theme Toggle
+const toggleBtn = document.getElementById("themeToggle");
+
+// Load saved theme
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark");
+    toggleBtn.textContent = "☀️";
+}
+
+// Toggle theme
+toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+        localStorage.setItem("theme", "dark");
+        toggleBtn.textContent = "☀️";
+    } else {
+        localStorage.setItem("theme", "light");
+        toggleBtn.textContent = "🌙";
+    }
 });
